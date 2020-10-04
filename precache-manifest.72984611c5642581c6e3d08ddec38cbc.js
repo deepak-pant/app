74933b3d63fe6a0fa8067fac7055295cd877c895
Deepak Pant <depak>
2020-10-04 18:50:07 +1100

    Updates

---
+++ precache-manifest.72984611c5642581c6e3d08ddec38cbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3987f8ac1d374ff1d6a334d740011f8",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "289f4d10bb721a491234",
+    "url": "/app/static/css/main.8bba1bc3.chunk.css"
+  },
+  {
+    "revision": "be18019dab560502be62",
+    "url": "/app/static/js/2.207abad3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.207abad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289f4d10bb721a491234",
+    "url": "/app/static/js/main.25c19f1e.chunk.js"
+  },
+  {
+    "revision": "16c996af4bbd4dbf5cef",
+    "url": "/app/static/js/runtime-main.03f8a4eb.js"
+  },
+  {
+    "revision": "a68d9df5330fee22d5338155a77bf904",
+    "url": "/app/static/media/dami.a68d9df5.jpg"
+  }
+]);
